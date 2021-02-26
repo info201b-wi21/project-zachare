9a66f609e1ea5f0e045a8df000bb5d21714c35ec
heatheryangg <77031025+heatheryangg@users.noreply.github.com>
2021-02-25 19:55:31 -0800

    Update exploratory_report_analysis.R

--- exploratory_report_analysis.R
+++ exploratory_report_analysis.R
@@ -1,1 +1,9 @@
+# Section 3
 
+most_recent_year <- health_expenditures %>%
+  filter(year == 2018) %>%
+  
+malnourishment <- malnourishment %>%
+  mutate(
+    malnourishment = sum(Severe.Wasting, Wasting, Overweight, Stunting, Underweight)
+  )
