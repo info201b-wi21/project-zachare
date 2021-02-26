e50851fa33a33f470bed81effe76b2883a90c3b5
heatheryangg <77031025+heatheryangg@users.noreply.github.com>
2021-02-25 20:37:58 -0800

    Update exploratory_report_analysis.R

--- exploratory_report_analysis.R
+++ exploratory_report_analysis.R
@@ -1,9 +1,32 @@
-# Section 3
+# Section 3: Heather
 
-most_recent_year <- health_expenditures %>%
-  filter(year == 2018) %>%
-  
-malnourishment <- malnourishment %>%
+most_recent_year <- health_expenditures$"2018"
+
+malnourishment <- na.omit(malnourishment) %>%
   mutate(
-    malnourishment = sum(Severe.Wasting, Wasting, Overweight, Stunting, Underweight)
+    total_malnourishment = Severe.Wasting + Wasting + Overweight + Stunting + Underweight
   )
+
+health_expenditures <- health_expenditures %>%
+  rename(
+    "Country" = Country.Name
+  )
+
+health_expenditures$Country <-  toupper(health_expenditures$Country)
+
+correlation_plot_df <- left_join(malnourishment, health_expenditures, by = c("Country")) %>%
+  select(Country, total_malnourishment, "2018")
+
+correlation_plot_df <- correlation_plot_df %>%
+  rename(
+    "health_expenditure_2018" = "2018"
+  )
+
+correlation_plot <- ggplot(correlation_plot_df, aes(x = health_expenditure_2018, y = total_malnourishment)) +
+  geom_point() +
+  ggtitle("Scatterplot of health expenditures and malnourishment rates") +
+  xlab("Health Expenditure (Current USD)") +
+  ylab("Malnourishment Rate") +
+  theme(
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank())
