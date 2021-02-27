03cc1a5d5d301473f87c928a8a31a7b0ebb864ae
heatheryangg <77031025+heatheryangg@users.noreply.github.com>
2021-02-26 18:20:58 -0800

    Update exploratory_report_analysis.R

--- exploratory_report_analysis.R
+++ exploratory_report_analysis.R
@@ -1,3 +1,37 @@
+# Section 2
+
+
+malnourishment_df <- read.csv("https://raw.githubusercontent.com/info201b-wi21/project-zachare/main/data/malnutrition-estimates%20(1).csv?token=ASLWDXDCP5ZFM5WZEXBKM5LAIHHAE")
+health_expenditures <- read.csv("https://raw.githubusercontent.com/info201b-wi21/project-zachare/main/data/Health%20Expenditures/ae2e0043-4b54-4d83-af00-191617f8402b_Data.csv?token=ASLWM4KWCSH5B33LJMJPOTTAHWFRW")
+
+library("dplyr")
+library("tidyverse")
+library("naniar")
+
+malnourishment_sample <- malnourishment_df %>%
+  top_n(5) %>%
+  select(Country, Wasting, Overweight, Underweight)
+
+health_expenditures <- health_expenditures %>%
+  rename(
+    "year_1990" = X1990..YR1990.,
+    "year_2000" = X2000..YR2000.,
+    "year_2011" = X2011..YR2011.,
+    "year_2012" = X2012..YR2012.,
+    "year_2013" = X2013..YR2013.,
+    "year_2014" = X2014..YR2014.,
+    "year_2015" = X2015..YR2015.,
+    "year_2016" = X2016..YR2016.,
+    "year_2017" = X2017..YR2017.,
+    "year_2018" = X2018..YR2018.,
+    "year_2019" = X2019..YR2019.,
+    "year_2020" = X2020..YR2020.
+  )
+
+health_expenditures_sample <- health_expenditures %>%
+  select(Country.Name, year_2017, year_2018) %>%
+  slice(1:3)
+
 # Section 3: Heather
 
 malnourishment_df_graph <- malnourishment_df %>%
