ad3a483751f6ee8cbd46d739e2f201c649a62a27
lizavolozin <77029852+lizavolozin@users.noreply.github.com>
2021-03-09 11:21:52 -0800

    Add files via upload
    initial commit

---
+++ app.R
@@ -0,0 +1,14 @@
+library('knitr')
+library('ggplot2')
+library('scales')
+library('maps')
+library('RColorBrewer')
+library('countrycode', exclude = "select")
+library('dplyr')
+library('tidyverse')
+library('naniar')
+
+source("my_ui.R")
+source("my_server.R")
+
+shinyApp(ui = ui, server = server)
---
+++ my_ui.R
@@ -0,0 +1,3 @@
+source("expenditures_and_malnourishment.R")
+
+ui <- navbarPage(strong("Health Expenditures and Malnourishment"), expenditures_and_malnourishment)
