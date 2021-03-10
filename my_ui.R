51ff189c8a7d5e17d4dfcec65b13ce4a4e56cade
lizavolozin <77029852+lizavolozin@users.noreply.github.com>
2021-03-09 20:55:35 -0800

    Update my_ui.R

--- my_ui.R
+++ my_ui.R
@@ -1,36 +1,40 @@
-source("expenditures_and_malnourishment.R")
+health_expenditures_and_malnourishment_info <- sidebarPanel(
+      sliderInput(
+        inputId = "plot_year",
+        label = h3("Year Range to Plot"),
+        min=2011, max=2018, value = c(2012, 2016), sep="", ticks=FALSE),
+      checkboxGroupInput(
+        inputId = "categories",
+        label = h3("Categories to Plot"),
+        selected = c("Wasting", "Overweight", "Underweight", "Stunting"), 
+        inline = FALSE, 
+        choices = c("Wasting", "Overweight", "Underweight", "Stunting")))
 
-ui <- fluidPage(
   
-  titlePanel("Health Expenditures and Malnourishment"),
+  health_expenditures_and_malnourishment <- tabPanel("Health Expenditures and Malnourishment", 
+                                                     sidebarLayout(
+                                                       health_expenditures_and_malnourishment_info, 
+                                                       mainPanel(plotOutput("correlation_plot"))
+                                                     ))
+  
+health_expenditures_over_time_info <- sidebarPanel(
+      sliderInput(
+        inputId = "year",
+        label = h3("Year Range to Plot"),
+        min = 2011, max = 2018, value = c(2011, 2018), sep="", ticks=FALSE),
+      checkboxGroupInput(
+        inputId = "countries",
+        label = h3("Countries to Plot"),
+        selected = c("World"), 
+        inline = FALSE, 
+        choices = c("World", "Russian Federation", "United Kingdom", "Germany", "France", "Japan", "Israel", "United States", "China", "India")))
+  
+  health_expenditures_over_time <- tabPanel("Health Expenditures Over Time", 
+                                                     sidebarLayout(
+                                                       health_expenditures_over_time_info, 
+                                                       mainPanel(plotOutput("country_trend_plot"))
+                                                     ))
+  
+ui <- navbarPage(strong("Health Expenditures and Malnourishment"), 
+                 health_expenditures_and_malnourishment, health_expenditures_over_time)
 
-    sidebarLayout(
-  
-      sidebarPanel(
-    
-        sliderInput(
-          inputId = "plot_year",
-          label = h3("Year Range to Plot"),
-          min=2011, max=2018, value = c(2012, 2016), sep="", ticks=FALSE
-        ),
-  
-        br(),
-  
-  
-        checkboxGroupInput(
-          inputId = "categories",
-          label = h3("Categories to Plot"),
-          selected = c("Wasting", "Overweight", "Underweight", "Stunting"), 
-          inline = FALSE, 
-          choices = c("Wasting", "Overweight", "Underweight", "Stunting")
-       )
-      ),
-       
-       mainPanel(
-         tabsetPanel(type = "tabs",
-                     tabPanel("Health Expenditures and Malnourishment", plotOutput("correlation_plot"))
-  )
-)
-)
-
-)
