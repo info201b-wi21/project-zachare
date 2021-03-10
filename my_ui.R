ce202e11a4a9f6be7024db159023950659e40ed2
lizavolozin <77029852+lizavolozin@users.noreply.github.com>
2021-03-09 19:16:56 -0800

    Update my_ui.R

--- my_ui.R
+++ my_ui.R
@@ -1,3 +1,28 @@
 source("expenditures_and_malnourishment.R")
 
-ui <- navbarPage(strong("Health Expenditures and Malnourishment"), expenditures_and_malnourishment)
+side_panel_info <- sidebarPanel(
+  sliderInput(
+    inputId = "plot_year",
+    label = h3("Year Range to Plot"),
+    min=2011, max=2018, value = c(2012, 2016), sep="", ticks=FALSE
+  ),
+  checkboxGroupInput(
+  inputId = "categories",
+  label = h3("Categories to Plot"),
+  selected = c("Wasting", "Overweight", "Underweight", "Stunting"), 
+  inline = FALSE, 
+  choices = c("Wasting", "Overweight", "Underweight", "Stunting")
+  )
+)
+
+
+malnourishment_plot <- tabPanel("Health Expenditures and Malnourishment",
+                                sidebarLayout(
+                                  side_panel_info,
+                                  mainPanel(
+                                    plotOutput("correlation_plot")
+                                  )
+                                )
+)
+
+ui <- navbarPage(strong("Health Expenditures and Malnourishment"), malnourishment_plot)
